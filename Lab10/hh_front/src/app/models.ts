58eb360515cc0b7ce254f1131207444435d96feb
M3ru3rt <124073395+M3ru3rt@users.noreply.github.com>
2023-04-19 17:22:38 +0600

    Lab10

---
+++ Lab10/hh_front/src/app/models.ts
@@ -0,0 +1,19 @@
+export interface Company {
+    id:number;
+    name:string;
+    description:string;
+    address:string;
+  }
+  
+  export interface Vacancy {
+    id:number;
+    name:string;
+    description:string;
+    salary:number;
+    company_id:number;
+  }
+  
+  export interface AuthToken {
+    token:string;
+  }
+  
